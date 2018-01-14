bd0a3cd640550ce11ab20af3178d0c75351bf2a5
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-01-14 10:13:13 +0100

    tests: Add tests for broken links

--- tests/links.spec.js
+++ tests/links.spec.js
@@ -1,33 +1,37 @@
+import fetch from 'node-fetch';
+
 describe('Links', () => {
   const sections = document.querySelectorAll('ul');
 
   sections.forEach((section, index) => {
     describe(`Section: ${index + 1}`, () => {
+      const links = section.querySelectorAll('a');
+      const hrefs = Array.from(links).map(l => l.href);
+      const texts = Array.from(links).map(l => l.text.toLowerCase());
+
       it('must be unique href', () => {
-        const links = section.querySelectorAll('a');
+        const unique = Array.from(new Set(hrefs));
 
-        const texts = Array.from(links).map(l => l.href);
-        const unique = Array.from(new Set(texts));
-
-        expect(texts).toEqual(unique);
+        expect(hrefs).toEqual(unique);
       });
 
       it('must be unique text', () => {
-        const links = section.querySelectorAll('a');
-
-        const texts = Array.from(links).map(l => l.text);
         const unique = Array.from(new Set(texts));
 
         expect(texts).toEqual(unique);
       });
 
       it('must be alphabetical order', () => {
-        const links = section.querySelectorAll('a');
-
-        const texts = Array.from(links).map(l => l.text.toLowerCase());
         const sorted = Array.from(texts).sort();
 
         expect(texts).toEqual(sorted);
+      });
+
+      expect.assertions(hrefs.length);
+      hrefs.forEach((href) => {
+        it('must reach ' + href, () => {
+          return fetch(href).then((res) => expect(res.ok).toEqual(true));
+        });
       });
     });
   });
